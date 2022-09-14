5f5e3d98d1b85263881d78437f31666008897bce
sehun.jang <sehun.jang@illuminarean.com>
2022-09-14 12:59:01 +0900

    update config

--- gvw-test-service/src/main/java/micro/gvwtestservice/ConfigController.java
+++ gvw-test-service/src/main/java/micro/gvwtestservice/ConfigController.java
@@ -1,7 +1,6 @@
 package micro.gvwtestservice;
 
 import lombok.RequiredArgsConstructor;
-import micro.gvwtestservice.config.DatabaseConfig;
 import micro.gvwtestservice.config.GvwConfig;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,12 +11,8 @@ public class ConfigController {
 
     private final GvwConfig serviceConfig;
 
-    private final DatabaseConfig databaseConfig;
-
     @GetMapping("/all")
     public String getAll() {
-        return serviceConfig.toString() + "\n"
-                + databaseConfig.toString()
-                ;
+        return serviceConfig.toString();
     }
 }
